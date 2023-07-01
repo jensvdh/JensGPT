b795f206ae19e9cb8382300e3edf2a382fe8c2ff
Jens Vanderhaeghe <jens.vanderhaeghe@airbnb.com>
2023-06-30 23:34:54 -0700

    Initial Commit

---
+++ JensGPT/JensGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  JensGPTApp.swift
+//  JensGPT
+//
+//  Created by Jens Vanderhaeghe on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct JensGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
