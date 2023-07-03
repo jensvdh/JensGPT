1cef58105fa23141aa40f9fde79766bede20ae7f
Jens Vanderhaeghe <jens.vanderhaeghe@airbnb.com>
2023-07-03 12:28:15 -0700

    Make window movable by dragging anywhere on the background
    - Add MovableWindow subclass of NSWindow that overrides isMovableByWindowBackground
    - Use MovableWindow to create window in AppDelegate
    - Remove WindowGroup from JensGPTApp
    - Use Settings scene with EmptyView instead
    - Set window background to clear

--- JensGPT/JensGPTApp.swift
+++ JensGPT/JensGPTApp.swift
@@ -1,26 +1,43 @@
 import SwiftUI
 import HotKey
 
+class MovableWindow: NSWindow {
+    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+        super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+        self.isMovableByWindowBackground = true
+        self.isOpaque = false
+        self.backgroundColor = .clear
+    }
+}
+
 @main
 struct JensGPTApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
-        .windowStyle(.hiddenTitleBar)
     }
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var hotKey: HotKey?
+    var window: NSWindow!
     
     func applicationDidFinishLaunching(_ notification: Notification) {
-        for window in NSApplication.shared.windows {
-            window.isOpaque = false
-            window.backgroundColor = .clear
-        }
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the window and set the content view.
+        window = MovableWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: contentView)
+        window.makeKeyAndOrderFront(nil)
         
         let hotKey = HotKey(key: .slash, modifiers: [.command])
         hotKey.keyDownHandler = {
